700474a44cf6a010d0c70d629c335aa18b7aa9ff
deepak <deepakkumardash313@gmail.com>
2022-10-23 20:24:03 +0530

    add : login and signup form

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 /** @format */
 
+import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
@@ -13,6 +15,10 @@ function MyApp({ Component, pageProps }) {
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
       <Component {...pageProps} />
     </SessionContextProvider>
   );
