634929fa3a68626a0bd434a1b772c93b7df61de0
deepak <deepakkumardash313@gmail.com>
2022-10-24 19:35:44 +0530

    add pwd update feat & logout fix

--- components/Account.js
+++ components/Account.js
@@ -5,6 +5,7 @@ import {
   useSupabaseClient,
   useUser,
 } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import FormInput from "./FormInput";
 
@@ -12,12 +13,15 @@ const Account = () => {
   const supabase = useSupabaseClient();
   const session = useSession();
   const user = useUser();
+  const router = useRouter();
 
   const [loading, setLoading] = useState(true);
   const [userName, setUserName] = useState(null);
   const [website, setWebsite] = useState(null);
   const [name, setName] = useState(null);
   const [profilePicUrl, setProfilePicUrl] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
 
   const getProfile = async () => {
     try {
@@ -72,6 +76,29 @@ const Account = () => {
     }
   };
 
+  const updatePassword = async () => {
+    try {
+      setLoading(true);
+      if (password !== confirmPassword) {
+        alert("Please confirm password");
+        return;
+      }
+      const { data, error } = await supabase.auth.updateUser({
+        password: password,
+      });
+      console.log({ data, error });
+
+      if (error) throw error;
+
+      alert("Password updated");
+    } catch (error) {
+      alert("Error updating password");
+      console.log({ error });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     session && getProfile();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -79,41 +106,77 @@ const Account = () => {
 
   return (
     <div className="container mt-3">
-      <FormInput name={"Email"} type="text" value={session?.user?.email} />
-      <FormInput name={"Name"} settervalue={setName} type="text" value={name} />
-      <FormInput
-        name={"Username"}
-        settervalue={setUserName}
-        type="text"
-        value={userName}
-      />
-      <FormInput
-        name={"Profile Picture URL"}
-        settervalue={setProfilePicUrl}
-        type="text"
-        value={profilePicUrl}
-      />
-      <FormInput
-        name={"Website"}
-        settervalue={setWebsite}
-        type="website"
-        value={website}
-      />
+      <div className="row">
+        <div className="col-6">
+          <FormInput name={"Email"} type="text" value={session?.user?.email} />
+          <FormInput
+            name={"Name"}
+            settervalue={setName}
+            type="text"
+            value={name}
+          />
+          <FormInput
+            name={"Username"}
+            settervalue={setUserName}
+            type="text"
+            value={userName}
+          />
+          <FormInput
+            name={"Profile Picture URL"}
+            settervalue={setProfilePicUrl}
+            type="text"
+            value={profilePicUrl}
+          />
+          <FormInput
+            name={"Website"}
+            settervalue={setWebsite}
+            type="website"
+            value={website}
+          />
 
-      <div>
-        <button
-          className="btn btn-primary"
-          onClick={() => updateProfile()}
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
-        </button>
-        <button
-          className="btn btn-danger ms-3"
-          onClick={() => supabase.auth.signOut()}
-        >
-          Logout
-        </button>
+          <div>
+            <button
+              className="btn btn-primary"
+              onClick={() => updateProfile()}
+              disabled={loading}
+            >
+              {loading ? "Loading ..." : "Update"}
+            </button>
+            <button
+              className="btn btn-danger ms-3"
+              onClick={async () => {
+                await supabase.auth.signOut();
+                router.push("/");
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+        <div className="col-6">
+          <FormInput
+            name={"Enter new Password"}
+            type="password"
+            value={password}
+            settervalue={setPassword}
+          />
+          <FormInput
+            name={"Confirm new Password"}
+            type="password"
+            value={confirmPassword}
+            settervalue={setConfirmPassword}
+          />
+
+          <div>
+            <button
+              className="btn btn-primary"
+              onClick={() => updatePassword()}
+              disabled={loading}
+            >
+              {loading ? "Loading ..." : "Update Password"}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
